5ed81a370830a8938d3fe57f48130f6e07e38a72
Rene Ismael <Ismael_br7@hotmail.com>
2019-10-29 00:05:18 -0600

    Proyecto clima react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import Error from "./components/Error";
+import Clima from "./components/Clima";
 
 function App() {
   const [ciudad, guardarCiudad] = useState("");
   const [pais, guardaPais] = useState("");
   const [error, guardaError] = useState(false);
+  const [resultado, guardaResultado] = useState({})
+
+  useEffect(() => {
+    
+    //prevenir ejecucion
+    if(ciudad === '') return;
+
+    const consultarAPI = async () => {
+
+      const appId = 'd931b49d74e4dce233ab711eb41fa6ad'
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+  
+      //consultar url
+  
+      const respuesta = await fetch(url);
+      const resultadoJSON = await respuesta.json();
+      
+      guardaResultado(resultadoJSON)
+  
+    }
+
+    consultarAPI();
+  }, [ciudad, pais])
 
   const datosConsulta = datos => {
     //validar que campos esten
@@ -17,18 +41,21 @@ function App() {
     }
 
     guardarCiudad(datos.ciudad);
-    guardarCiudad(datos.pais);
+    guardaPais(datos.pais);
     guardaError(false);
   };
 
   //Cargar un componente condicionalmente
-
   let componente;
   if (error) {
     //hay un error, mostrarlo
     componente = <Error mensaje="Ambos campos son obligatorios" />;
-  } else {
-    componente = null;
+  } 
+  else if(resultado.cod === '404'){
+    componente = <Error mensaje="La ciudad no existe en nuestro registro" />
+  } 
+  else {
+    componente = <Clima resultado={resultado} />;
   }
 
   return (
